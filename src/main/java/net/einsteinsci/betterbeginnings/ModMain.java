3d88e9cadb02defe65f3ed2fdfbba7b8e05e698a
Leviathan143 <Leviathan143@users.noreply.github.com>
2017-09-10 08:46:53 +1200

    Build b0.9.6-R2a

--- src/main/java/net/einsteinsci/betterbeginnings/ModMain.java
+++ src/main/java/net/einsteinsci/betterbeginnings/ModMain.java
@@ -27,7 +27,7 @@ import net.minecraftforge.common.config.Configuration;
 public class ModMain
 {
 	public static final String MODID = "betterbeginnings";
-	public static final String VERSION = "0.9.6-R1a";
+	public static final String VERSION = "b0.9.6-R2a";
 	public static final String NAME = "BetterBeginnings";
 	public static final CreativeTabs tabBetterBeginnings = new CreativeTabs("tabBetterBeginnings")
 	{
